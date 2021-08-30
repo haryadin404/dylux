823aaa728a6c00749520769fe0963ec1c400de4e
holafg <89357793+holafg@users.noreply.github.com>
2021-08-29 22:37:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -52,14 +52,24 @@ Fg.version = [2, 2119, 6]
 //enku.browserDescription = [ '[•] Fg', 'Chrome', '3.0' ]
 
 
-/*Fg.logger.level = 'warn'
-console.log(color(figlet.textSync(`${spc1}FG98`, {
+Fg.logger.level = 'warn'
+/*console.log(color(figlet.textSync(`${spc1}FG98`, {
 font: 'Standard',
 horizontalLayout: 'default',
 vertivalLayout: 'default',
 width: 80,
 whitespaceBreak: false
-}), 'cyan'))*/
+}), 'cyan'))
+
+
+console.log(color(`${spc2}DyLux By FG98 Ig : @fg98._` ,'cyan'))
+console.log(color(`${spc4}< ================================================== >`, 'cyan'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Nama        : DyLux`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Bot Version : 4.0.1`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Status      : Online!`, 'white'))
+console.log(color(`${spc3}[•]`, 'aqua'), color(`Owner       : ${ownerN}`, 'white'))
+console.log(color(`${spc4}< ================================================== >`, 'cyan'))
+*/
 
 const CFonts  = require('cfonts')
 CFonts.say('FG98', {
@@ -73,13 +83,6 @@ CFonts.say(`DyLux By FG98 Ig : @fg98._`, {
   colors: ['yellow']
 })
 
-console.log(color(`${spc2}DyLux By FG98 Ig : @fg98._` ,'cyan'))
-console.log(color(`${spc4}< ================================================== >`, 'cyan'))
-console.log(color(`${spc3}[•]`, 'aqua'), color(`Nama        : DyLux`, 'white'))
-console.log(color(`${spc3}[•]`, 'aqua'), color(`Bot Version : 4.0.1`, 'white'))
-console.log(color(`${spc3}[•]`, 'aqua'), color(`Status      : Online!`, 'white'))
-console.log(color(`${spc3}[•]`, 'aqua'), color(`Owner       : ${ownerN}`, 'white'))
-console.log(color(`${spc4}< ================================================== >`, 'cyan'))
 
 Fg.on('qr', () => {
 console.log(color('[','white'), color('!','cyan'), color(']','white'), color('Scan Now This Qr!'))
